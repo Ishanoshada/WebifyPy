a358819fa50a8390cfd710334042850db5163373
Ishan Oshada <63636533+Ishanoshada@users.noreply.github.com>
2023-10-25 18:00:07 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='webify',
+    version='0.1.0',
+    author='Ishan Oshada',
+    author_email='ic31908@gmail.com',
+    description='A Python package for web development',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ishanoshada/webify',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Operating System :: OS Independent',
+    ],
+    project_urls={
+        'Bug Reports': 'https://github.com/ishanoshada/webify/issues',
+        'Source': 'https://github.com/ishan oshada/webify',
+    },
+    keywords='web development, Python package ',
+    license='MIT',
+)
